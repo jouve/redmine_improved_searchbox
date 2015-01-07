6386fd73064ed9a3ad995f5f6044ca2f8be73a1c
Berk Demirkır <bdemirkir@users.noreply.github.com>
2015-01-07 14:59:52 +0200

    Disable Select2 on mobile
    Proposed fix for #9

--- assets/javascripts/redmine_improved_searchbox.js
+++ assets/javascripts/redmine_improved_searchbox.js
@@ -1,7 +1,9 @@
 $(function(){
-	var str = $('#project_quick_jump_box option:first').text();
-	$('#project_quick_jump_box option:first').text("").val("");
-	$('#project_quick_jump_box').attr("data-placeholder", str).select2({ width: function() {
-		return ($('#project_quick_jump_box').width() + 32).toString() + 'px';
-	} });
+	if ('ontouchstart' in window || !!(navigator.msMaxTouchPoints)) {
+		var str = $('#project_quick_jump_box option:first').text();
+		$('#project_quick_jump_box option:first').text("").val("");
+		$('#project_quick_jump_box').attr("data-placeholder", str).select2({ width: function() {
+			return ($('#project_quick_jump_box').width() + 32).toString() + 'px';
+		}});
+	}
 });
