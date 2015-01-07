91435ce877805e2276f5fe21c770da26d81506df
Berk Demirkır <bdemirkir@users.noreply.github.com>
2015-01-07 15:02:06 +0200

    Reverse mobile check logic

--- assets/javascripts/redmine_improved_searchbox.js
+++ assets/javascripts/redmine_improved_searchbox.js
@@ -1,9 +1,10 @@
 $(function(){
-	if ('ontouchstart' in window || !!(navigator.msMaxTouchPoints)) {
-		var str = $('#project_quick_jump_box option:first').text();
-		$('#project_quick_jump_box option:first').text("").val("");
-		$('#project_quick_jump_box').attr("data-placeholder", str).select2({ width: function() {
-			return ($('#project_quick_jump_box').width() + 32).toString() + 'px';
-		}});
-	}
+	if ('ontouchstart' in window || !!(navigator.msMaxTouchPoints))
+		return;
+
+	var str = $('#project_quick_jump_box option:first').text();
+	$('#project_quick_jump_box option:first').text("").val("");
+	$('#project_quick_jump_box').attr("data-placeholder", str).select2({ width: function() {
+		return ($('#project_quick_jump_box').width() + 32).toString() + 'px';
+	}});
 });
